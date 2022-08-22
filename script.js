a5d59b488fb86ae589841e7b33376f2d33015fdb
makoma mabilo <makoma44@gmail.com>
2022-08-22 16:01:30 +0800

    cleaning up js script

--- script.js
+++ script.js
@@ -11,19 +11,6 @@ function normalFont() {
     change.style.fontSize = "30px";
 }
 
-
-const colourchange = document.getElementById("flex-image-1");
-colourchange.addEventListener("mouseover", () => changeColour());
-colourchange.addEventListener("mouseout", () => normalColour());
-
-function changeColour() {
-    colourchange.style.color = "#F4BA13";
-}
-
-function normalColour() {
-    colourchange.style.color = "white";
-}
-//
 const changefont2 = document.getElementById("flex-image-2");
 changefont2.addEventListener("mouseover", () => changeFont2());
 changefont2.addEventListener("mouseout", () => normalFont2());
@@ -36,17 +23,6 @@ function normalFont2() {
     changefont2.style.fontSize = "30px";
 }
 
-const colourchange1 = document.getElementById("flex-image-2");
-colourchange1.addEventListener("mouseover", () => changeColour1());
-colourchange1.addEventListener("mouseout", () => normalColour1());
-
-function changeColour1() {
-    colourchange1.style.color = "#F4BA13";
-}
-
-function normalColour1() {
-    colourchange1.style.color = "white";
-}
 //
 const changefont3 = document.getElementById("flex-image-3");
 changefont3.addEventListener("mouseover", () => changeFont3());
@@ -60,17 +36,6 @@ function normalFont3() {
     changefont3.style.fontSize = "30px";
 }
 
-const colourchange2 = document.getElementById("flex-image-3");
-colourchange2.addEventListener("mouseover", () => changeColour2());
-colourchange2.addEventListener("mouseout", () => normalColour2());
-
-function changeColour2() {
-    colourchange2.style.color = "#F4BA13";
-}
-
-function normalColour2() {
-    colourchange2.style.color = "white";
-}
 //Row 2 images
 const changefont4 = document.getElementById('flex-image-4');
 changefont4.addEventListener("mouseover", () => changeFont4());
@@ -84,17 +49,6 @@ function normalFont4() {
     changefont4.style.fontSize = "30px";
 }
 
-const imagechange1 = document.getElementById('flex-image-4');
-imagechange1.addEventListener("mouseover", () => imageTransform1());
-imagechange1.addEventListener("mouseout", () => defaultImage1());
-
-function imageTransform1() {
-    imagechange1.style.backgroundImage ="url('https://images.unsplash.com/photo-1540778930632-ac8ee7468936?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')";
-}
-
-function defaultImage1() {
-    imagechange1.style.backgroundImage="url('https://images.unsplash.com/photo-1496105463139-c6c6f14dedf7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE5fHxtYWxpJTIwYXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60')";
-}
 
 const changefont5 = document.getElementById("flex-image-5");
 changefont5.addEventListener("mouseover", () => changeFont5());
@@ -108,17 +62,6 @@ function normalFont5() {
     changefont5.style.fontSize = "30px";
 }
 
-const imagechange2 = document.getElementById("flex-image-5");
-imagechange2.addEventListener("mouseover", () => imageTransform2());
-imagechange2.addEventListener("mouseout", () => defaultImage2());
-
-function imageTransform2() {
-    imagechange2.style.backgroundImage = "url('https://images.unsplash.com/photo-1565472459197-82ba571cc915?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1181&q=80')";
-}
-
-function defaultImage2() {
-    imagechange2.style.backgroundImage = "url('https://images.unsplash.com/photo-1610023709598-3881e09811c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGFyaXMlMjBhcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60')";
-}
 //
 const changefont6 = document.getElementById("flex-image-6");
 changefont6.addEventListener("mouseover", () => changeFont6());
@@ -132,17 +75,6 @@ function normalFont6() {
     changefont6.style.fontSize = "30px";
 }
 
-const imagechange3 = document.getElementById("flex-image-6");
-imagechange3.addEventListener("mouseover", () => imageTransform3());
-imagechange3.addEventListener("mouseout", () => defaultImage3());
-
-function imageTransform3() {
-    imagechange3.style.backgroundImage = "url('https://images.unsplash.com/photo-1530894787135-b65ecfd505bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80')";
-}
-
-function defaultImage3() {
-    imagechange3.style.backgroundImage = "url('https://images.unsplash.com/photo-1530975080071-4d6704a9a993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')";
-}
 //row 3
 const changefont7 = document.getElementById("flex-image-7");
 changefont7.addEventListener("mouseover", () => changeFont7());
@@ -156,16 +88,6 @@ function normalFont7() {
     changefont7.style.fontSize = "30px";
 }
 
-const colourchange3 = document.getElementById("flex-image-7");
-colourchange3.addEventListener("mouseover", () => changeColour3());
-colourchange3.addEventListener("mouseout", () => normalColour3());
-
-function changeColour3() {
-    colourchange3.style.color = "#F4BA13";
-}
-   function normalColour3() {
-    colourchange3.style.color = "white";
-   }
 //
 const changefont8 = document.getElementById("flex-image-8");
 changefont8.addEventListener("mouseover", () => changeFont8());
@@ -178,19 +100,6 @@ function changeFont8() {
 function normalFont8() {
     changefont8.style.fontSize = "30px";
 }
-
-const colourchange4 = document.getElementById("flex-image-8");
-colourchange4.addEventListener("mouseover", () => changeColour4());
-colourchange4.addEventListener("mouseout", () => normalColour4());
-
-function changeColour4() {
-    colourchange4.style.color = "#F4BA13";
-
-}
-
- function normalColour4() {
-    colourchange4.style.color = "white";
-   }
 //
 const changefont9 = document.getElementById("flex-image-9");
 changefont9.addEventListener("mouseover", () => changeFont9());
@@ -203,18 +112,6 @@ function changeFont9() {
 function normalFont9() {
     changefont9.style.fontSize = "30px";
 }
-
-const colourchange5 = document.getElementById("flex-image-9");
-colourchange5.addEventListener("mouseover", () => changeColour5());
-colourchange5.addEventListener("mouseout", () => normalColour5());
-
-function changeColour5() {
-    colourchange5.style.color = "#F4BA13";
-}
-
- function normalColour5() {
-    colourchange5.style.color = "white";
-   }
 //row 4 images
 const changefonta = document.getElementById("flex-image-10");
 changefonta.addEventListener("mouseover", () => changeFonta());
@@ -228,21 +125,7 @@ function normalFonta() {
     changefonta.style.fontSize = "30px";
 }
 
-const imagechange4 = document.getElementById("flex-image-10");
-imagechange4.addEventListener("mouseover", () => imageTransform4());
-imagechange4.addEventListener("mouseout", () => defaultImage4());
 
-function imageTransform4() {
-    imagechange4.style.backgroundImage = "url('https://images.unsplash.com/photo-1557599175-d26391c0cf33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')";
-}
-
-function defaultImage4() {
-    imagechange4.style.backgroundImage = "url('https://images.unsplash.com/photo-1550407590-fe5359dd2b6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTMyfHxiYWxpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60')";
-}
-//
-const changefontb = document.getElementById("flex-image-11");
-changefontb.addEventListener("mouseover", () => changeFontb());
-changefontb.addEventListener("mouseout", () => normalFontb());
 
 function changeFontb() {
     changefontb.style.fontSize = "40px";
@@ -252,17 +135,6 @@ function normalFontb() {
     changefontb.style.fontSize = "30px";
 }
 
-const imagechange5 = document.getElementById("flex-image-11");
-imagechange5.addEventListener("mouseover", () => imageTransform5());
-imagechange5.addEventListener("mouseout", () => defaultImage5());
-
-function imageTransform5() {
-    imagechange5.style.backgroundImage = "url('https://images.unsplash.com/photo-1601027192343-7d787c2e5367?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')";
-}
-
-function defaultImage5() {
-    imagechange5.style.backgroundImage = "url('https://images.unsplash.com/photo-1583267575071-d13239e37986?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')";
-}
 //
 const changefontc = document.getElementById("flex-image-12");
 changefontc.addEventListener("mouseover", () => changeFontc());
@@ -276,17 +148,7 @@ function normalFontc() {
     changefontc.style.fontSize = "30px";
 }
 
-const imagechange6 = document.getElementById("flex-image-12");
-imagechange6.addEventListener("mouseover", () => imageTransform6());
-imagechange6.addEventListener("mouseout", () => defaultImage6());
 
-function imageTransform6() {
-    imagechange6.style.backgroundImage = "url('https://images.unsplash.com/photo-1580746738099-1cb74f972feb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2535&q=80')";
-}
-
-function defaultImage6() {
-    imagechange6.style.backgroundImage = "url('https://images.unsplash.com/photo-1615841192203-84f7b2899c0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')";
-}
 //row 5
 const changefontd = document.getElementById("flex-image-13");
 changefontd.addEventListener("mouseover", () => changeFontd());
@@ -299,18 +161,6 @@ function changeFontd() {
 function normalFontd() {
     changefontd.style.fontSize = "30px";
 }
-
-const colourchange6 = document.getElementById("flex-image-13");
-colourchange6.addEventListener("mouseover", () => changeColour6());
-colourchange6.addEventListener("mouseout", () => normalColour6());
-
-function changeColour6() {
-    colourchange6.style.color = "#F4BA13";
-}
-
- function normalColour6() {
-    colourchange6.style.color = "white";
-   }
 //
 const changefonte = document.getElementById("flex-image-14");
 changefonte.addEventListener("mouseover", () => changeFonte());
@@ -324,17 +174,6 @@ function normalFonte() {
     changefonte.style.fontSize = "30px";
 }
 
-const colourchange7 = document.getElementById("flex-image-14");
-colourchange7.addEventListener("mouseover", () => changeColour7());
-colourchange7.addEventListener("mouseout", () => normalColour7());
-
-function changeColour7() {
-    colourchange7.style.color = "#F4BA13";
-}
-
- function normalColour7() {
-    colourchange7.style.color = "white";
-   }
 //
 const changefontf = document.getElementById("flex-image-15");
 changefontf.addEventListener("mouseover", () => changeFontf());
@@ -348,17 +187,7 @@ function normalFontf() {
     changefontf.style.fontSize = "30px";
 }
 
-const colourchange8 = document.getElementById("flex-image-15");
-colourchange8.addEventListener("mouseover", () => changeColour8());
-colourchange8.addEventListener("mouseout", () => normalColour8());
 
-function changeColour8() {
-    colourchange8.style.color = "#F4BA13";
-}
-
- function normalColour8() {
-    colourchange8.style.color = "white";
-   }
 //row 6
 const changefontg = document.getElementById("flex-image-16");
 changefontg.addEventListener("mouseover", () => changeFontg());
@@ -372,17 +201,6 @@ function normalFontg() {
     changefontg.style.fontSize = "30px";
 }
 
-const imagechange7 = document.getElementById("flex-image-16");
-imagechange7.addEventListener("mouseover", () => imageTransform7());
-imagechange7.addEventListener("mouseout", () => defaultImage7());
-
-function imageTransform7() {
-    imagechange7.style.backgroundImage = "url('https://images.unsplash.com/photo-1513434007132-241c9f0ecb56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')";
-}
-
-function defaultImage7() {
-    imagechange7.style.backgroundImage = "url('https://images.unsplash.com/photo-1564863757083-ada48bf46a8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80')";
-}
 //
 const changefonth = document.getElementById("flex-image-17");
 changefonth.addEventListener("mouseover", () => changeFonth());
@@ -396,17 +214,6 @@ function normalFonth() {
     changefonth.style.fontSize = "30px";
 }
 
-const imagechange8 = document.getElementById("flex-image-17");
-imagechange8.addEventListener("mouseover", () => imageTransform8());
-imagechange8.addEventListener("mouseout", () => defaultImage8());
-
-function imageTransform8() {
-    imagechange8.style.backgroundImage = "url('https://images.unsplash.com/photo-1527838832700-5059252407fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2596&q=80')";
-}
-
-function defaultImage8() {
-    imagechange8.style.backgroundImage = "url('https://images.unsplash.com/photo-1621165264791-d110a730e930?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')";
-}
 //
 const changefonti = document.getElementById("flex-image-18");
 changefonti.addEventListener("mouseover", () => changeFonti());
@@ -420,17 +227,7 @@ function normalFonti() {
     changefonti.style.fontSize = "30px";
 }
 
-const imagechange9 = document.getElementById("flex-image-18");
-imagechange9.addEventListener("mouseover", () => imageTransform9());
-imagechange9.addEventListener("mouseout", () => defaultImage9());
 
-function imageTransform9() {
-    imagechange9.style.backgroundImage = "url('https://images.unsplash.com/photo-1559542356-462fbb19438f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80')";
-}
-
-function defaultImage9() {
-    imagechange9.style.backgroundImage = "url('https://images.unsplash.com/photo-1530633866995-0ad6c385894d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1245&q=80')";
-}
 //row 7
 const changefontj = document.getElementById("flex-image-19");
 changefontj.addEventListener("mouseover", () => changeFontj());
@@ -444,17 +241,6 @@ function normalFontj() {
     changefontj.style.fontSize = "30px";
 }
 
-const colourchange9 = document.getElementById("flex-image-19");
-colourchange9.addEventListener("mouseover", () => changeColour9());
-colourchange9.addEventListener("mouseout", () => normalColour9());
-
-function changeColour9() {
-    colourchange9.style.color = "#F4BA13";
-}
-
- function normalColour9() {
-    colourchange9.style.color = "white";
-   }
 //
 const changefontk = document.getElementById("flex-image-20");
 changefontk.addEventListener("mouseover", () => changeFontk());
@@ -467,18 +253,6 @@ function changeFontk() {
 function normalFontk() {
     changefontk.style.fontSize = "30px";
 }
-
-const colourchange10 = document.getElementById("flex-image-20");
-colourchange10.addEventListener("mouseover", () => changeColour10());
-colourchange10.addEventListener("mouseout", () => normalColour10());
-
-function changeColour10() {
-    colourchange10.style.color = "#F4BA13";
-}
-
- function normalColour10() {
-    colourchange10.style.color = "white";
-   }
 //
 const changefontl = document.getElementById("flex-image-21");
 changefontl.addEventListener("mouseover", () => changeFontl());
@@ -491,15 +265,3 @@ function changeFontl() {
 function normalFontl() {
     changefontl.style.fontSize = "30px";
 }
-
-const colourchange11 = document.getElementById("flex-image-21");
-colourchange11.addEventListener("mouseover", () => changeColour11());
-colourchange11.addEventListener("mouseout", () => normalColour11());
-
-function changeColour11() {
-    colourchange11.style.color = "#F4BA13";
-}
-
- function normalColour11() {
-    colourchange11.style.color = "white";
-   }
